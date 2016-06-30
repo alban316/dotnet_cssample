5655da3e6fe80093e4ad9b17f1209978ae6f1509
alban316 <jason.brugger@gmail.com>
2016-06-30 09:59:32 -0400

    initial commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            /* Hi Temps for Sydney, AU */
+            Dictionary<DateTime, double> hi_temp = new Dictionary<DateTime, double>{
+                {DateTime.Parse("6/23/16"), 19d},
+                {DateTime.Parse("6/24/16"), 17d},
+                {DateTime.Parse("6/25/16"), 14d},
+                {DateTime.Parse("6/26/16"), 15d},
+                {DateTime.Parse("6/27/16"), 11d},
+                {DateTime.Parse("6/28/16"), 15d},
+                {DateTime.Parse("6/29/16"), 18d}
+            };
+
+            double daytemp = hi_temp.Where(t => t.Key == DateTime.Parse("6/25/16")).FirstOrDefault().Value;
+
+            Console.WriteLine(daytemp);
+        }
+    }
+}
---
+++ readme.txt
@@ -0,0 +1,11 @@
+To run as portable:
+
+dotnet /bin/Debug/netcoreapp1.0/cssample.dll
+
+Creating Self-Contained apps:
+
+https://docs.microsoft.com/en-us/dotnet/articles/core/app-types#self-contained-application
+
+Runtime IDentifier (RID):
+
+https://docs.microsoft.com/en-us/dotnet/articles/core/rid-catalog#what-are-rids
